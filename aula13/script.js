d09125e767b8609b3e2e7d0ba63a765cf41279d2
anehellyperrony <153025974+Anehelly@users.noreply.github.com>
2024-02-20 11:57:01 -0300

    criei o exercício 13

--- aula13/script.js
+++ aula13/script.js
@@ -1,1 +1,19 @@
-alert('Olá')
+function tabuada(){
+    var n = window.document.getElementById('num')
+    var n1 = Number(n.value)
+    var res = window.document.getElementById('res')
+
+    if(n.value === ''){
+        alert('Informe o número para iniciar a tabuada!')
+        return
+    }
+
+    res.innerHTML = 'Iniciando a tabuada...';
+
+    for(var c=1; c<=10; c++){
+        var conta = n1*c
+        res.innerHTML += '<br>' + n1 + 'x' + c + '=' + conta + '<br>'
+        
+        
+    }
+}
