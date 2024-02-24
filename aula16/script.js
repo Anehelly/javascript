0eb6602291ab044fe3b2cb3f516cf5b23e926d7a
anehellyperrony <153025974+Anehelly@users.noreply.github.com>
2024-02-23 23:17:15 -0300

    exercício 16

---
+++ aula16/script.js
@@ -0,0 +1,18 @@
+
+    let n = document.getElementById('num')
+    let n1 = Number(n.value)
+    let lista = document.getElementById('flista')
+    let res = document.getElementById('res')
+
+    
+    var valores = []
+
+    function adicionar(){
+        
+    }
+
+   
+
+    for(var cont=0; cont<=(numeros.length); cont++){
+        res.innerHTML += numeros[cont] + '<br>'; // Adiciona cada número seguido de u
+        }
